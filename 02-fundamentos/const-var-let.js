edbbff8293e7ba51bfb44c4daa2821d145fa4e93
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-03 21:33:04 +0100

    Ejemplo CONST

--- 02-fundamentos/const-var-let.js
+++ 02-fundamentos/const-var-let.js
@@ -1,4 +1,15 @@
-//LET
+//CONST
+const nombre = 'Wolverine';
+console.log(nombre);
+
+if(true){
+    nombre = 'Magneto' //en este punto tiene que fallar ya que no se puede cambiar el valor de una constante
+    console.log(nombre);
+}
+
+console.log(nombre);
+
+/* //LET
 let nombre = 'Wolverine';
 console.log(nombre);
 
@@ -7,7 +18,7 @@ if(true){
     console.log(nombre);
 }
 
-console.log(nombre);
+console.log(nombre); */
 
 /* //VAR
 var nombre = 'Wolverine';
