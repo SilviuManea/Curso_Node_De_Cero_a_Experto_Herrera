24b9b1ed6c729fa4d19a5cbefd9ed6af87ba3606
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-04 11:28:03 +0100

    02-18-ejemplo destructuración dentro de una función

--- 02-fundamentos/destructuracion.js
+++ 02-fundamentos/destructuracion.js
@@ -5,19 +5,26 @@ const deadpool = {
     poder: 'Regeneración',
     edad: 50,
     getNombre() {
-        return `${ this.nombre } ${ this.apellido } ${ this.poder }`;
+        return `${this.nombre} ${this.apellido} ${this.poder}`;
     }
 }
 
-console.log ( deadpool.getNombre() );
+//console.log(deadpool.getNombre());
 
 // Sacar los valores de Manera manual
 // const nombre = deadpool.nombre;
 // const apellido = deadpool.apellido;
 // const poder = deadpool.poder;
 
-// Sacar los valores usando destructuración
-const {nombre , apellido , poder, edad = 0} = deadpool;// si edad tiene un valor inicial se le asigna ese valor(50) y si no, el valor por defecto 0
-console.log(nombre, apellido, poder,edad);
+// Destructurar dentro de la función
+function imprimeHeroe(heroe) {
 
-    
+    // Sacar los valores usando destructuración
+    const { nombre, apellido, poder, edad = 0 } = deadpool;
+    console.log(nombre, apellido, poder, edad);
+
+}
+imprimeHeroe(deadpool);
+
+
+
