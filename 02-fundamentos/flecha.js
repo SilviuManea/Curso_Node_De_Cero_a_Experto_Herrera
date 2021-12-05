4091c098ac182eb0172e372464af5c56842c37b0
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-05 10:50:27 +0100

    Ejemplo funciones de flecha

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,15 @@
+
+// Funciones tradicionales
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+// Funciones de flecha
+const sumar = (a, b) => {
+    return a + b;
+}
+console.log(sumar(5, 10));
+
+// Función de flecha de una sola línea
+const saludar = () => 'Hola Mundo';
+console.log(saludar());
