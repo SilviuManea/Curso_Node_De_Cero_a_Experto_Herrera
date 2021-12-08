04348feae2cddf32af092110e4c8f438cfbfb654
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-08 10:44:47 +0100

    Ejemplo Promesa

---
+++ 02-fundamentos/promesas.js
@@ -0,0 +1,46 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Silviu'
+    },
+    {
+        id: 2,
+        nombre: 'Eduardo'
+    },
+    {
+        id: 3,
+        nombre: 'Fernando'
+    },
+]
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    },
+]
+
+const getEmpleado = (id) => {
+
+    //el callback resolve se llamará cuando devuelva un resultado y el reject cuando ocurra un error
+    const promesa = new Promise((resolve, reject) => {
+        //cuerpo de la promesa busca un empleado por id
+        const empleado = empleados.find(e => e.id === id)
+
+        if (empleado) {
+            resolve(empleado);
+        } else {
+            reject(`No existe empleado con id ${id}`);
+        }
+    });
+    //devolvemos la promesa
+    return promesa;
+}
+const id = 8;
+getEmpleado(id)
+    .then(empleado => console.log(empleado))
+    .catch(err => console.log(err));
