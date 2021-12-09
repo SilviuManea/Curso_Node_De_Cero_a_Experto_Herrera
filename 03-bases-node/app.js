d6e451bd9f95bccb83f343323842fc00d5783ea1
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-09 21:34:57 +0100

    Ejemplo uso require con fs para hacer la tabla del 5

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+
 console.clear();
 console.log('===================')
 console.log('====Tabla del 5====')
 console.log('===================')
 
 const base = 5;
+let salida = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} x ${i}`);
+    salida += (`${base} x ${i}\n`);
 }
+console.log(salida);
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log(`tabla-${base}.txt creada`);
+});
