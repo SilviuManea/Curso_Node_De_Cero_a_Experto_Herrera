dbfb3d4a60cc2d2cd0a7f6151cda94255bfdfff0
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-13 20:42:44 +0100

    Ejemplo uso argumentos consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,14 @@
 const { crearArchivo } = require("./helpers/multiplicar");
 
-const base = 5;
+
+console.clear();
+
+//Destructuración de un array -> guarda cada elemento del array en una constante
+const [, , arg3 = 'base=5'] = process.argv; // los primeros dos elementos no nos interesan, y si no nos dan ningun valor le ponemos 5 por defecto
+const [, base] = arg3.split('='); // separamos el string por el = y nos quedamos con el segundo argumento(el número)
+//console.log(base);
 
 //crearArchivo(base);
-
-//Ejercicio - hacer que trabaje como una promesa.
-
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, ' creado'))
     .catch(err => console.log(err));
