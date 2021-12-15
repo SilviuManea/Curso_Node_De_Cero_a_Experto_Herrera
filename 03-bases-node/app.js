e23c83e5f403dde737b5e1eb3439d439fb0e0437
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-15 22:39:18 +0100

    Prettier instalado,y ejemplo de como separar la config del yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,31 +1,6 @@
-const { crearArchivo } = require("./helpers/multiplicar");
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number', // tirará NaN si no es un número pero seguirá.
-        demandOption: true
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: false,
-        default: false
-    })
-    .check((argv, options) => {//validación adicional para que no siga.
-        //console.log('yargs', argv)
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un número'
-        }
-        return true;//si no hay ningún error
-    })
-    .argv;
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
 
-//console.clear();
-
-//console.log(process.argv);
-//console.log(argv.listar);
-
-//crearArchivo(base);
 crearArchivo(argv.base, argv.listar)
-    .then(nombreArchivo => console.log(nombreArchivo, ' creado'))
-    .catch(err => console.log(err));
+  .then((nombreArchivo) => console.log(nombreArchivo, ' creado'))
+  .catch((err) => console.log(err));
---
+++ 03-bases-node/config/yargs.js
@@ -0,0 +1,25 @@
+const argv = require('yargs')
+  .option('b', {
+    alias: 'base',
+    type: 'number', // tirará NaN si no es un número pero seguirá.
+    demandOption: true,
+    describe: 'Es la base de la tabla de multiplicar.',
+  })
+  .option('l', {
+    alias: 'listar',
+    type: 'boolean',
+    demandOption: false,
+    default: false,
+    describe: 'Muestra la tabla en consola.',
+  })
+  .check((argv, options) => {
+    //validación adicional para que no siga.
+    //console.log('yargs', argv)
+    if (isNaN(argv.b)) {
+      throw 'La base tiene que ser un número';
+    }
+    return true; //si no hay ningún error
+  }).argv;
+
+//lo exportamos para poder usarlo en el main
+module.exports = argv;
