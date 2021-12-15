9d708ec0415f495ab38741946cdb7767e7097aaf
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-15 22:53:34 +0100

    Ejemplo uso colores en consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,9 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
+const colors = require('colors');
 
 crearArchivo(argv.base, argv.listar)
-  .then((nombreArchivo) => console.log(nombreArchivo, ' creado'))
+  .then((nombreArchivo) =>
+    console.log(nombreArchivo.rainbow, ' creado'),
+  )
   .catch((err) => console.log(err));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,37 +1,35 @@
 const { error } = require('console');
+const colors = require('colors');
 const fs = require('fs');
 
-const crearArchivo = async (base = 5, listar = false) => { // si no nos pasan una base por defecto se le va a asignar el 5
+const crearArchivo = async (base = 5, listar = false) => {
+  // si no nos pasan una base por defecto se le va a asignar el 5
 
-
-    try {
-
-        //console.log(listar);
-        let salida = '';
-        for (let i = 1; i <= 10; i++) {
-            salida += (`${base} x ${i}\n`);
-        }
-
-        if (listar) {
-            //console.clear();
-            console.log('===================')
-            console.log('   Tabla del:', base)
-            console.log('===================')
-            console.log(salida);
-
-        }
-
-        fs.writeFileSync(`tabla-${base}.txt`, salida);
-        //Ejercicio - devolver ek nombre del archivo si se hizo correctamente
-        return (`tabla-${base}.txt`);
-
-    } catch (error) {
-        throw error;
+  try {
+    //console.log(listar);
+    let salida = '';
+    for (let i = 1; i <= 10; i++) {
+      salida += `${base} ${'x'.green} ${i} ${'='.green} ${
+        base * i
+      }\n`;
     }
 
-}
+    if (listar) {
+      //console.clear();
+      console.log('==================='.green);
+      console.log('   Tabla del:'.green, colors.blue(base));
+      console.log('==================='.green);
+      console.log(salida);
+    }
+
+    fs.writeFileSync(`tabla-${base}.txt`, salida);
+    //Ejercicio - devolver ek nombre del archivo si se hizo correctamente
+    return `tabla-${base}.txt`;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    crearArchivo
-}
-
+  crearArchivo,
+};
