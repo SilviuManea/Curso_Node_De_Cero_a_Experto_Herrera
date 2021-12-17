c0a9ce4b59e2d06232f9178fa535fd33194a9e2c
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-17 11:28:43 +0100

    Funcionalidad pausa - sin implementar aún

--- 04-tareas-hacer/helpers/mensajes.js
+++ 04-tareas-hacer/helpers/mensajes.js
@@ -21,11 +21,29 @@ const mostrarMenu = () => {
   });
 
   readline.question('Seleccione una opción:', (opt) => {
-    console.log({ opt });
+    //console.log({ opt });
     readline.close();
   });
 };
+
+//preparamos el códgo para poner en pausa la aplciación(cuando el usuario está en el menú principal)
+const pausa = () => {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question(
+    `\nPresione ${'ENTER'.green} para continuar\n`,
+    (opt) => {
+      //console.log({ opt });
+      readline.close();
+    },
+  );
+};
+
 //exportamos el módulo para usarlo desde fuera.
 module.exports = {
   mostrarMenu,
+  pausa,
 };
