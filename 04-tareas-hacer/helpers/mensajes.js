3ac56ee523ad7e74abdfa1c37aeb03444dbb1421
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2021-12-17 11:09:23 +0100

    Menú actualizado

--- 04-tareas-hacer/helpers/mensajes.js
+++ 04-tareas-hacer/helpers/mensajes.js
@@ -4,7 +4,15 @@ const mostrarMenu = () => {
   console.clear();
   console.log('====================='.green);
   console.log('Seleccione una opción');
-  console.log('====================='.green);
+  console.log('=====================\n'.green);
+
+  console.log(`${'1.'.green} Crear tarea`);
+  console.log(`${'2.'.green} Listar tarea`);
+  console.log(`${'3.'.green} Listar tareas completadas`);
+  console.log(`${'4.'.green} Listar tareas pendientes`);
+  console.log(`${'5.'.green} Completar tarea(s)`);
+  console.log(`${'6.'.green} Borrar tarea`);
+  console.log(`${'0.'.green} Salir \n`);
 };
 
 //exportamos el módulo para usarlo desde fuera.
