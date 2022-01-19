8d7324c911fd6a6ae7665ba03b19670c2fe507b2
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-01-19 21:53:44 +0100

    Ejemplo Menu de la aplicacion

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,9 +1,25 @@
 const { leerImput } = require('./helpers/inquirer');
+const { inquireMenu, pausa } = require('./helpers/inquirer');
 
 const main = async () => {
-  const texto = await leerImput('Hola: ');
+  //console.log('Hola Mundo');
+  let opt = '';
 
-  console.log(texto);
+  do {
+    opt = await inquireMenu();
+
+    switch (opt) {
+      case 1:
+        const desc1 = await leerImput('Descripción 1:');
+        console.log(desc1);
+        break;
+      case 2:
+        const desc2 = await leerImput('Descripción 2:');
+        console.log(desc2);
+        break;
+    }
+    await pausa();
+  } while (opt !== 0);
 };
 
 main();
