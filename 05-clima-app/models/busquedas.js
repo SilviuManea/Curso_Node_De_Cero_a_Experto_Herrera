6f262877cc25b11bc4c950133a6f2465f8b51e0f
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-01-25 20:35:02 +0100

    Ejemplo petición basica mapbox

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -12,7 +12,7 @@ class Busquedas {
       //console.log('Ciudad', lugar);
       //peticion http
       const resp = await axios.get(
-        'https://reqres.in/api/users?page=2',
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/Zaragoza.json?language=es&access_token=pk.eyJ1IjoibGFtZW50ZW9jdXBhZGEiLCJhIjoiY2t5dWhvY25nMW9kdzJxcHQ3Z2t6aTdxZSJ9.dWtN5thVHVIHXwoesGYz-Q',
       );
       console.log(resp.data);
 
