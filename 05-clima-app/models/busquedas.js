75b6fe9ae3f260b9d4ccd92a49746f3afcbff28c
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-01-25 22:36:39 +0100

    Ejemplo uso axios para parametrizar la petición a al api

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -7,15 +7,24 @@ class Busquedas {
     //TODO: leer DB si existe
   }
 
+  get paramsMapbox() {
+    return {
+      language: 'es',
+      access_token:
+        'pk.eyJ1IjoibGFtZW50ZW9jdXBhZGEiLCJhIjoiY2t5dWhvY25nMW9kdzJxcHQ3Z2t6aTdxZSJ9.dWtN5thVHVIHXwoesGYz-Q',
+      limit: 5,
+    };
+  }
+
   async ciudad(lugar = '') {
     try {
-      //console.log('Ciudad', lugar);
       //peticion http
-      const resp = await axios.get(
-        'https://api.mapbox.com/geocoding/v5/mapbox.places/Zaragoza.json?language=es&access_token=pk.eyJ1IjoibGFtZW50ZW9jdXBhZGEiLCJhIjoiY2t5dWhvY25nMW9kdzJxcHQ3Z2t6aTdxZSJ9.dWtN5thVHVIHXwoesGYz-Q',
-      );
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox,
+      });
+      const resp = await instance.get();
       console.log(resp.data);
-
       return []; //retornar los lugares que coincidan con la busqueda en curso
     } catch (error) {
       console.log(error);
