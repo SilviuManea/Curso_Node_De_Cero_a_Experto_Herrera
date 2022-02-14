99282123d4e9a2c763d2f196a5278c29b02a330a
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-02-14 23:07:26 +0100

    Ejemplo express basado en clases

---
+++ 07-restserver/models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi apliación
+    this.routes();
+  }
+
+  middlewares() {
+    // Directorio publico
+    this.app.use(express.static('public'));
+  }
+
+  routes() {
+    this.app.get('/api', (req, res) => {
+      res.send('Hello World');
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor lanzado en puerto ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
