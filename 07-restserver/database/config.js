148c7b5530873ba22b026fd7b594dc132eb2dd6b
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-03-03 20:19:40 +0100

    Ejemplo conexión bd usando mongoose

---
+++ 07-restserver/database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_ATLAS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //   useCreateIndex: false,
+      //   useFindAndModify: false,
+    });
+    console.log('Base de datos online.');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error a la hora de conectar a la BD');
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
