e377eed0c627b852ee34b914cca27c62acdee6bd
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-04-21 17:30:37 +0200

    UPD - Sobreescribir un método para eliminar campos

--- 07-restserver/models/usuario.js
+++ 07-restserver/models/usuario.js
@@ -32,6 +32,12 @@ const UsuarioSchema = Schema({
   },
 });
 
+// Sobreescribir un método para que haga algo que nosotros queremos(eliminar el password y la versión)
+UsuarioSchema.methods.toJSON = function (){ //ojo tiene que ser una función normal , sin flecha, esto es asi porque una funcion de flecha mantiene a lo que apunta el this fuera de la misma y nosotros necesitamos hacer referencia a la instancia creada, por lo que se usa la función normal.
+  const {__v,password, ...usuario} = this.toObject(); // esto genera la instancia como objeto literal de javascript. SACA los campos __v y password, y usando el operador rest(...) guarda el resto de campos en la variable usuario.
+  return usuario;//devolvemos el objeto menos esos dos campos que acabamos de quitar
+}
+
 // Exportamos Usuario que será el nombre que tendrá nuestra coleccion
 // Mongoogse le Añade una s al final. Por lo que se llamará Usuarios
 
