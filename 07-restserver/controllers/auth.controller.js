55ce36c262adfa37e9f92088bf8c54c72955eca5
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-04-26 21:21:07 +0200

    UPD - Login del usuario

--- 07-restserver/controllers/auth.controller.js
+++ 07-restserver/controllers/auth.controller.js
@@ -1,14 +1,51 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
-const login = (req , res = response) => {
+const login = async (req, res = response) => {
+  const { correo, password } = req.body; //capturamos los datos del body de la request
 
+  try {
+    // Verificar si el email existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - correo',
+      });
+    }
+
+    // Verificar si el usuario está activo(estado)
+    if (!usuario.estado) {
+      // si estado === false
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - estado:false',
+      });
+    }
+
+    // Verificar la contraseña(devuelve true si matchea o false si no.)
+    const validPassword = bcryptjs.compareSync(
+      password,
+      usuario.password,
+    );
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - password',
+      });
+    }
+
+    // Generar el JWT
 
     res.json({
-        msg: 'Login ok'
-    })
-
-}
+      msg: 'Login ok',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Hable con el administrador',
+    });
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+};
