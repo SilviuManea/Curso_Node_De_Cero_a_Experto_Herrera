e0a7e1e4861af768fe34d84f835461cb8d4f3ae3
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-04-26 21:56:36 +0200

    UPD-Cambiar _id por uid

--- 07-restserver/models/usuario.js
+++ 07-restserver/models/usuario.js
@@ -35,7 +35,8 @@ const UsuarioSchema = Schema({
 // Sobreescribir un método para que haga algo que nosotros queremos(eliminar el password y la versión)
 UsuarioSchema.methods.toJSON = function () {
   //ojo tiene que ser una función normal , sin flecha, esto es asi porque una funcion de flecha mantiene a lo que apunta el this fuera de la misma y nosotros necesitamos hacer referencia a la instancia creada, por lo que se usa la función normal.
-  const { __v, password, ...usuario } = this.toObject(); // esto genera la instancia como objeto literal de javascript. SACA los campos __v y password, y usando el operador rest(...) guarda el resto de campos en la variable usuario.
+  const { __v, password, _id, ...usuario } = this.toObject(); // esto genera la instancia como objeto literal de javascript. SACA los campos __v y password, y usando el operador rest(...) guarda el resto de campos en la variable usuario.
+  usuario.uid = _id; //estamos renombrando visualmente el campo, pero el valor se mantiene
   return usuario; //devolvemos el objeto menos esos dos campos que acabamos de quitar
 };
 
