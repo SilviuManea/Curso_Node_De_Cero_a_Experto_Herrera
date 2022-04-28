1826f5cd7918297c94cbcfb69a86b2ae0d75a4b0
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-04-28 14:20:11 +0200

    UPD - Usuario de google frontend

--- 07-restserver/routes/usuarios.route.js
+++ 07-restserver/routes/usuarios.route.js
@@ -9,17 +9,18 @@ const {
   usuariosDelete,
   usuariosPatch,
 } = require('../controllers/usuarios.controller');
-const { validarCampos } = require('../middlewares/validar-campos');
 const {
   esRoleValido,
   emailExiste,
   existeUsuarioPorId,
 } = require('../helpers/db-validators');
-const { validarJWT } = require('../middlewares/validar-jwt');
-const {
-  esAdminRole,
-  tieneRole,
-} = require('../middlewares/validar-roles');
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const {
+//   esAdminRole,
+//   tieneRole,
+// } = require('../middlewares/validar-roles');
+const {validarCampos,validarJWT,esAdminRole,tieneRole} = require ('../middlewares');
 
 const router = Router();
 
