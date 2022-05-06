03b88ea2565d59846653a5391a7b978a1f1d408c
Silviu Manea Hamzau <silviu.manea.hamzau@outlook.com>
2022-05-06 22:37:19 +0200

    UPD-Sequelize

--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,49 +1,58 @@
 import cors from 'cors';
 import express, { Application } from 'express';
+import db from '../db/connection';
 import userRoutes from '../routes/usuario.route';
 
 class Server {
+  private app: Application;
+  private port: string;
+  private apiPaths = {
+    usuarios: '/api/usuarios',
+  };
 
-    private app:Application;
-    private port:string;
-    private apiPaths = {
-        usuarios: '/api/usuarios'
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || '8000'; // Se pone por defecto el 8000 porque this.port no puede ser undefined
+
+    // Conexión con la BD
+    this.dbConnection();
+    // Habilitar el middleware
+    this.middlewares();
+    // Definir mis rutas y habilitarlas
+    this.routes();
+  }
+
+  // DB connection
+
+  async dbConnection() {
+    try {
+      await db.authenticate();
+      console.log('Db Online');
+    } catch (error: any) {
+      throw new Error(error);
     }
+  }
 
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT || '8000' // Se pone por defecto el 8000 porque this.port no puede ser undefined
-        // Habilitar el middleware
-        this.middlewares();
-        // Definir mis rutas y habilitarlas
-        this.routes();
-    }
+  middlewares() {
+    // CORS
+    this.app.use(cors());
 
-    // TODO:COnectar la base de datos
+    // Lectura del body
+    this.app.use(express.json()); // Express va a parsearnos a json lo que venga en el body
 
-    middlewares() {
+    // Carpeta pública
+    this.app.use(express.static('public'));
+  }
 
-        // CORS
-        this.app.use(cors());
+  routes() {
+    this.app.use(this.apiPaths.usuarios, userRoutes);
+  }
 
-        // Lectura del body
-        this.app.use(express.json()); // Express va a parsearnos a json lo que venga en el body
-
-        // Carpeta pública
-        this.app.use(express.static('public'));
-
-    }
-
-    routes(){
-        this.app.use(this.apiPaths.usuarios,userRoutes)
-    }
-
-    listen(){
-        this.app.listen(this.port, ()=> {
-            console.log('Servidor corriendo en el puerto '+ this.port);
-        })
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Servidor corriendo en el puerto ' + this.port);
+    });
+  }
 }
 
 export default Server;
